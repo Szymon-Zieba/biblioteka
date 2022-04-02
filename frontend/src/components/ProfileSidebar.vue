15a4ee5a741ad9d56cb753040ad58225164fdb2d
Bartomiej Sobolewski <bs113774@stud.ur.edu.pl>
2022-04-02 12:47:47 +0200

    Menu profilu BIB-12

---
+++ frontend/src/components/ProfileSidebar.vue
@@ -0,0 +1,40 @@
+<script>
+export default {
+  props: ["width"],
+  setup(props) {
+    return {
+      props,
+    };
+  },
+};
+</script>
+
+<template>
+  <v-card>
+    <v-layout>
+      <v-navigation-drawer
+        :width="props.width"
+        permanent
+        style="background: burlywood"
+      >
+        <v-card-title>Profil</v-card-title>
+        <v-divider></v-divider>
+        <v-list nav style="background: inherit">
+          <v-list-item prepend-icon="mdi-account" title="Szczegóły konta">
+          </v-list-item>
+          <v-list-item
+            prepend-icon="mdi-bookshelf"
+            title="Wypożyczenia"
+          ></v-list-item>
+        </v-list>
+        <template v-slot:append>
+          <div class="pa-2">
+            <v-btn to="/logout" block color="brown"> Wyloguj </v-btn>
+          </div>
+        </template>
+      </v-navigation-drawer>
+    </v-layout>
+  </v-card>
+</template>
+
+<style></style>
