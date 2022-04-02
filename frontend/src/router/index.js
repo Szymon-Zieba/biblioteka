4b70f4693fdfd43599849e56cd5fbc4acb79b5a7
Bartomiej Sobolewski <bs113774@stud.ur.edu.pl>
2022-04-03 00:59:28 +0200

    Header dla zalogowanych i niezal. BIB-27

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,38 +1,45 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import LoginView from '../views/LoginView.vue'
-import RegisterView from '../views/RegisterView.vue'
-import HomeView from '../views/HomeView.vue'
-import BookView from '../views/BookView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import LoginView from "../views/LoginView.vue";
+import RegisterView from "../views/RegisterView.vue";
+import HomeView from "../views/HomeView.vue";
+import BookView from "../views/BookView.vue";
+import ProfileView from "../views/ProfileView.vue";
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
+    path: "/",
+    name: "home",
     component: HomeView,
     meta: { requiresAuth: false },
   },
   {
-    path: '/login',
-    name: 'login',
+    path: "/login",
+    name: "login",
     component: LoginView,
     meta: { requiresAuth: false },
   },
   {
-    path: '/register',
-    name: 'register',
-    component: RegisterView
+    path: "/register",
+    name: "register",
+    component: RegisterView,
   },
   {
-    path: '/books',
-    name: 'books',
+    path: "/books",
+    name: "books",
     component: BookView,
     meta: { requiresAuth: false },
   },
-]
+  {
+    path: "/profile",
+    name: "profile",
+    component: ProfileView,
+    meta: { requiresAuth: true },
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
