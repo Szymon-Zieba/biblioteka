e921b47de8db89e290fae99adb4c039c5e8cd02e
Izabela Szybiak <is113782@stud.ur.edu.pl>
2022-04-05 20:05:51 +0200

    BIB-21 Zarzadzanie ksiazkami 2

--- frontend/src/components/AdminBookMenegment.vue
+++ frontend/src/components/AdminBookMenegment.vue
@@ -73,14 +73,40 @@ export default {
     return { 
       books,
     }
-    }
+    },
 
-}
+  name2: 'add',
+  data(){
+    return{
+      showModal: false
+    }
+  },
+
+
+  data1: () => ({
+      valid: true,
+      title: '', 
+      author: '',
+      description: '',
+      select: null,
+      items: [
+        'Bajka',
+        'Dramat',
+        'Fantasy',
+        'Historyczna',
+      ]
+    }),
+
+    methods: {
+      add () {
+        this.$refs.form.add()
+      },
+    },
+  }
+  
 </script>
 
-<template>
-
-         
+<template>     
   <div class="booksAdmin"> 
     <div class="title">
         <h1> Lista książek </h1>
@@ -104,20 +130,82 @@ export default {
 
   <div class="add">
     <v-btn 
+    class="button"
       variant="outlined"
       size="large"
       icon
       color="#462e26"
+      @click="showModal = true"
     >
-      <v-icon>mdi-plus</v-icon>
+      <v-icon size="x-large">mdi-plus</v-icon>
     </v-btn>
+    <transition name="fade" appear>
+      <div class="modal-overlay" v-if="showModal" @click = "showModal = false"></div>
+    </transition>
+    <transition  name="slide" appear>
+      <div class="modal" v-if="showModal">
+        <button class="button" @click="showModal = false">
+          <v-icon>mdi-close</v-icon>
+        </button>
+        <h1 class="modalh1">Książka</h1>
+         <v-form
+          ref="form"
+          v-model="valid"
+          lazy-validation
+        >
+          <v-text-field
+            v-model="title"
+            :counter="50"
+            label="Tytuł"
+            required
+          ></v-text-field>
+
+          <v-text-field
+            v-model="author"
+            :counter="32"
+            label="Autor"
+            required
+          ></v-text-field>
+
+          <v-text-field
+            v-model="description"
+            label="Opis"
+            required
+          ></v-text-field>
+
+          <v-select
+            v-model="select"
+            :items="items"
+            label="Gatunek"
+            required
+          ></v-select>
+
+          <v-file-input
+          show-size
+          counter
+          multiple
+          label="Obraz"
+        ></v-file-input>
+
+          <v-btn
+            :disabled="!add"
+            color="brown"
+            class="mr-4"
+            @click="showModal = false"
+          >
+            Dodaj
+          </v-btn>
+        </v-form>
+      </div>
+    </transition>
   </div> 
+
 
   <v-row>
     <v-col >
       <v-card >
         <v-list lines="two"
-        v-for="book in books" :key="book.id">
+        v-for="book in books" :key="book.id" @click="showModal= true">
   <!--        <v-list-subheader>Dostępne książki</v-list-subheader>-->
   <!-- dodawanie awataru w list-item prepend-avatar="src/assets/img/bookicon.jpg"-->
           <v-list-item
@@ -128,20 +216,20 @@ export default {
             </template>
             <template v-slot:subtitle>
               <div class="des"><span class="font-weight-bold">{{book.genre}}</span> &mdash; {{book.text}} </div>
+              <div class="trash"><v-icon>mdi-delete</v-icon></div>
               <div class="id">{{book.id}}</div>
               <div class="author">{{book.author}}</div>
               <div style="clear:both;"></div>
             </template>
             
-            
           </v-list-item>
           <v-divider inset></v-divider>
-
         </v-list>
       </v-card>
     </v-col>
   </v-row>
   </div>
+  
 </template>
 
 
@@ -160,19 +248,81 @@ export default {
 .des{
   float: left; 
   width: 690px;
+  
 }
 .author{
+  
   float: right; 
-  width: 200px;
+  width: 180px;
   text-align: center;
   margin-left: 20px;
 }
 .id{
   float: right; 
-  width: 150px;
+  width: 120px;
   text-align: center;
   margin-left: 20px;
-  
+}
+.trash{
+  float: right; 
+  width: 20px;
+  text-align: center;
+  margin-left: 10px;
+  color: red; 
 }
 
+.modal-overlay {
+  position: absolute;
+ top: 0;
+ left: 0;
+ right: 0;
+ bottom: 0;
+ z-index: 98;
+ background-color: rgba(0, 0, 0, 0.3);
+}
+
+.modal {
+ position: fixed;
+ top: 50%;
+ left: 50%;
+ transform: translate(-50%, -50%);
+ z-index: 99;
+ 
+ width: 100%;
+ max-width: 400px;
+ background-color: #FFF;
+ border-radius: 16px;
+ 
+ padding: 25px;
+
+
+}
+.modalh1 {
+  color:  #462e26;
+  font-size: 32px;
+  font-weight: 900;
+  margin-bottom: 15px;
+  text-align: left;
+ }
+
+
+.fade-enter-active,
+.fade-leave-active {
+ transition: opacity .5s;
+}
+
+.fade-enter,
+.fade-leave-to {
+ opacity: 0;
+}
+
+.slide-enter-active,
+.slide-leave-active {
+ transition: transform .5s;
+}
+
+.slide-enter,
+.slide-leave-to {
+ transform: translateY(-50%) translateX(100vw);
+}
 </style>
