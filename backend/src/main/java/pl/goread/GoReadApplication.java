d402adaa6c65aee306d4241519c86416895e5d16
Bartomiej Sobolewski <bs113774@stud.ur.edu.pl>
2022-04-11 23:13:25 +0200

    BIB-29 Utworzenie projektu

---
+++ backend/src/main/java/pl/goread/GoReadApplication.java
@@ -0,0 +1,13 @@
+package pl.goread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoReadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoReadApplication.class, args);
+    }
+
+}
