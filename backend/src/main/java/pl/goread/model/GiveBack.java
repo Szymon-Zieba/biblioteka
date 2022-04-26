79a5cc9daf9f32dd9281c4ba25f439779b764cb6
Krystian Janas <kj113730@stud.ur.edu.pl>
2022-04-26 18:30:34 +0200

    BIB-40 - zmiana nazwy z Return.java na GiveBack.java

--- backend/src/main/java/pl/goread/model/GiveBack.java
+++ backend/src/main/java/pl/goread/model/GiveBack.java
@@ -3,13 +3,11 @@ package pl.goread.model;
 import lombok.Data;
 
 import javax.persistence.*;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.Set;
 
 @Entity
 @Data
-public class Return {
+public class GiveBack {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
