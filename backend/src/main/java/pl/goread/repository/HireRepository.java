a084ca52cb53ebb587bffc90edf2b67f9dd75e56
Krystian Janas <kj113730@stud.ur.edu.pl>
2022-04-26 23:09:23 +0200

    Poprawa BIB-49 - @repository

--- backend/src/main/java/pl/goread/repository/HireRepository.java
+++ backend/src/main/java/pl/goread/repository/HireRepository.java
@@ -1,12 +1,14 @@
 package pl.goread.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import pl.goread.model.Hire;
 import pl.goread.model.Library;
 import pl.goread.model.User;
 
 import java.util.List;
 
+@Repository
 public interface HireRepository extends JpaRepository<Hire, Long> {
 
     List<Hire> getHiresByLibrary(Library library);
