45eb8cdcd4dba92eb5a345e16446ea98f2cc3546
Bartomiej Sobolewski <bs113774@stud.ur.edu.pl>
2022-04-28 19:47:32 +0200

    BIB-65 Endpoint /users

--- backend/src/main/java/pl/goread/controller/UserController.java
+++ backend/src/main/java/pl/goread/controller/UserController.java
@@ -1,2 +1,26 @@
-package pl.goread.controller;public class UserController {
+package pl.goread.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.goread.model.User;
+import pl.goread.repository.UserRepository;
+import pl.goread.service.UserService;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    private final UserRepository userRepository;
+
+    @GetMapping("/users")
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+
 }
