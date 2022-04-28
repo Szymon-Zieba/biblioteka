438b1cb08936dbafd48ad373caac09b0fb0b1d27
Bartomiej Sobolewski <bs113774@stud.ur.edu.pl>
2022-04-28 20:07:17 +0200

    BIB-66 Endpoint /user/{id}

--- backend/src/main/java/pl/goread/controller/UserController.java
+++ backend/src/main/java/pl/goread/controller/UserController.java
@@ -2,17 +2,18 @@ package pl.goread.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.goread.model.User;
 import pl.goread.repository.UserRepository;
 import pl.goread.service.UserService;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 public class UserController {
+
 
     private final UserService userService;
 
@@ -21,6 +22,21 @@ public class UserController {
     @GetMapping("/users")
     public List<User> getUsers() {
         return userRepository.findAll();
+    }
+
+    @GetMapping("/user/{id}")
+    public User getUser(@PathVariable Long id){
+        return userService.getUserById(id);
+    }
+
+    @PutMapping("/user/{id}")
+    public User updateUser(@PathVariable Long id, @RequestBody User updatedUser){
+        return userService.updateUser(userService.getUserById(id), updatedUser);
+    }
+
+    @DeleteMapping("/user/{id}")
+    public void deleteUser(@PathVariable Long id){
+        userRepository.delete(userService.getUserById(id));
     }
 
 }
--- backend/src/main/java/pl/goread/service/UserService.java
+++ backend/src/main/java/pl/goread/service/UserService.java
@@ -1,15 +1,21 @@
 package pl.goread.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.BeanUtils;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import pl.goread.model.User;
 import pl.goread.repository.UserRepository;
+
+import javax.persistence.EntityNotFoundException;
 
 @Service
 @RequiredArgsConstructor
 public class UserService implements UserDetailsService {
+
+    private static final String USER_NOT_FOUND_MSG = "Nie znaleziono takiego użytkownika";
 
     private final UserRepository userRepository;
 
@@ -24,5 +30,16 @@ public class UserService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         return userRepository.findUserByEmail(email);
+    }
+
+    public User getUserById(Long id){
+        return userRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException(USER_NOT_FOUND_MSG));
+    }
+
+    public User updateUser(User user, User updatedUser){
+        BeanUtils.copyProperties(updatedUser, user);
+        userRepository.save(user);
+        return user;
     }
 }
