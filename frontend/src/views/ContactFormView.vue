da139c29e92fb2966fde745e591ba372e2dc7a8b
Szymon Zieba <sz113799@stud.ur.edu.pl>
2022-05-18 19:29:18 +0200

    BIB-101

--- frontend/src/views/ContactFormView.vue
+++ frontend/src/views/ContactFormView.vue
@@ -1,16 +1,63 @@
 <script>
+import { reactive} from "vue";
+
   export default {
-    data () {
-      return {
-        select: ['Vuetify', 'Programming'],
-        items: [
-          'Programming',
-          'Design',
-          'Vue',
-          'Vuetify',
-        ],
-      }
-    },
+    setup(){
+        const libraries = reactive([
+            {
+                id: "1",
+                mail: "dsadas@dasdas.com",
+                adress: "Rzeszów Ul.Cicha 56",
+                phone: "103-323-312",
+            },
+            {
+                id: "2",
+                mail: "dsadas@dasdas.com",
+                adress: "Bźdiszewo Ul.Martwa 58",
+                phone: "103-323-312",
+            },
+            {
+                id: "3",
+                mail: "dsadas@dasdas.com",
+                adress: "Rzeszów Ul.Cicha 56",
+                phone: "103-323-312",
+            },
+            {
+                id: "4",
+                mail: "dsadas@dasdas.com",
+                adress: "Rzeszów Ul.Cicha 56",
+                phone: "103-323-312",
+            },
+            {
+                id: "5",
+                mail: "dsadas@dasdas.com",
+                adress: "Białystok Ul.Cicha 56",
+                phone: "103-323-312",
+            },
+            {
+                id: "6",
+                mail: "dsadas@dasdas.com",
+                adress: "Poznań Ul.Cicha 56",
+                phone: "103-323-312",
+            },
+            {
+                id: "7",
+                mail: "dsadas@dasdas.com",
+                adress: "Kraków Ul.Cicha 56",
+                phone: "103-323-312",
+            },
+        ])
+
+        const getCityFromAdress = function(library){
+            const splited = library.adress.split(' ')
+            return splited[0]
+        }
+
+        return {
+            libraries,
+            getCityFromAdress
+        }
+    }
   }
 </script>
 
@@ -21,14 +68,14 @@
                 <router-link to="/">
                     <v-img
                         src="@/assets/img/logo-library.png"
-                        height="5rem"
+                        height="9rem"
                     ></v-img>
                 </router-link>
             </div>
             <v-container fluid>
                 <v-row>
                     <v-col cols="12">
-                        <v-row>
+                        <v-row class="font-size">
                             <v-col>
                                 <v-icon
                                     large
@@ -36,7 +83,7 @@
                                     >
                                     mdi-email
                                 </v-icon>
-                                <span>dsadasdasdsa</span>
+                                <span  class="text">biblioteka@gmail.com</span>
                             </v-col>
                             <v-col>
                                 <v-icon
@@ -45,28 +92,77 @@
                                     >
                                     mdi-domain
                                 </v-icon>
-                                <span>dsadasdasdsa</span>
+                                <span  class="text">Rzeszów ul.Piłsudzkiego 13</span>
                             </v-col>
                             <v-col>
                                 <v-icon
                                     large
-                                    color="yellow darken-2"
+                                    color="orange darken-2"
                                     >
                                     mdi-phone-classic
                                 </v-icon>
-                                <span>dsadasdasdsa</span>
+                                <span  class="text">
+                                    644-322-123
+                                </span>
                             </v-col>
                         </v-row>
                     </v-col>
                     <v-col cols="12">
-                        <v-expansion-panels>
-                            <v-expansion-panel
-                                v-for="i in 5"
-                                :key="i"
-                                title="Item"
-                                text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-                            >
-
+                        <v-expansion-panels style="font-size: 1.3rem;">
+                             <v-expansion-panel
+                                v-for="library in libraries"
+                                :key="library"
+                                style="padding: 0.5rem;"
+                             >
+                                <v-expansion-panel-title>
+                                    <v-row no-gutters>
+                                        <v-col cols="4" class="d-flex justify-start" style="font-size: 1.5rem;">
+                                        Biblioteka: {{ getCityFromAdress(library) }}
+                                        </v-col>
+                                        <v-col
+                                        cols="8"
+                                        class="text-black"
+                                        >
+                                        </v-col>
+                                    </v-row>
+                                </v-expansion-panel-title>
+                                <v-expansion-panel-text>
+                                    <v-row >
+                                        <v-col>
+                                            <v-icon
+                                                large
+                                                color="teal darken-2"
+                                                >
+                                                mdi-email
+                                            </v-icon>
+                                            <span  class="text">
+                                                {{ library.mail }}
+                                            </span>
+                                        </v-col>
+                                        <v-col>
+                                            <v-icon
+                                                large
+                                                color="green darken-2"
+                                                >
+                                                mdi-domain
+                                            </v-icon>
+                                            <span  class="text">
+                                                {{ library.adress }}
+                                            </span>
+                                        </v-col>
+                                        <v-col>
+                                            <v-icon
+                                                large
+                                                color="orange darken-2"
+                                                >
+                                                mdi-phone-classic
+                                            </v-icon>
+                                            <span  class="text">
+                                                {{ library.phone }}
+                                            </span>
+                                        </v-col>
+                                    </v-row>
+                                </v-expansion-panel-text>
                             </v-expansion-panel>
                         </v-expansion-panels>
                     </v-col>
@@ -97,15 +193,10 @@
     margin-bottom: 2rem;
 }
 
-.form{
-    color: white;
+.font-size{
+    font-size: 2rem;
 }
-.button{
-    width: 100%;
-    display: flex;
-    justify-content: space-around;
-}
-.btn{
-    padding: 3rem;
+.text{
+    padding: 0.5rem;
 }
 </style>
