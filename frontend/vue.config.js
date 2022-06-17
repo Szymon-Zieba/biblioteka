c7885a53306e1392c3e0b499cc4a5f14f0dc1b46
Kuba <kubaskaw000@gmail.com>
2022-06-18 00:13:06 +0200

    naprawiony blad ze sciezka

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,18 +1,17 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-
   configureWebpack: {
     devServer: {
-      historyApiFallback: true
-    }
+      historyApiFallback: true,
+    },
   },
 
   transpileDependencies: true,
-  publicPath: '',
+  publicPath: "/",
 
   pluginOptions: {
     vuetify: {
-			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
-		}
-  }
-})
+      // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
+    },
+  },
+});
